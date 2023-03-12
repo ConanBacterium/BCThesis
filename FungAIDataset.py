06d7d455ee07207ddcb33c292788d59e585d9680
Jacob Rohde <jas.rohde@gmail.com>
2023-03-12 14:58:14 +0100

    Add option for balancing dataset (pos, neg)

--- FungAIDataset.py
+++ FungAIDataset.py
@@ -4,17 +4,31 @@ import torch
 from torch.utils.data import Dataset
 from pathlib import Path 
 from PIL import Image
-import mysql.connector
+# import mysql.connector
 from get_annotation_pandas_df import get_annotation_pandas_df
 
 class FungAIDataset(Dataset):
-    def __init__(self, transform=None, limit=0):
+    def __init__(self, transform=None, limit=0, balanced=False):
         self.transform = transform
         self.annotations = get_annotation_pandas_df()
         self.annotations = self.annotations.dropna()
         self.annotations = self.annotations.reset_index(drop=True)
         
+        if balanced: 
+            self._balance_annotations(666)
+            sanity_check = self.annotations['Hyfer'][self.annotations['Hyfer'] > 0].count() == self.annotations['Hyfer'][self.annotations['Hyfer'] == 0].count()
+            print(f"balanced: {sanity_check}")
         if limit: self.annotations = self.annotations.iloc[0:limit]
+            
+    def _balance_annotations(self, randomSeed):
+        num_nonzeros = self.annotations['Hyfer'][self.annotations['Hyfer'] > 0].count()
+        num_zeros = self.annotations['Hyfer'][self.annotations['Hyfer'] == 0].count()
+        while num_nonzeros != num_zeros:
+            num_to_remove = num_zeros - num_nonzeros
+            remove_indices = self.annotations[self.annotations['Hyfer'] == 0].sample(n=num_to_remove, random_state=randomSeed).index
+            self.annotations = self.annotations.drop(remove_indices)
+            num_nonzeros = self.annotations['Hyfer'][self.annotations['Hyfer'] > 0].count()
+            num_zeros = self.annotations['Hyfer'][self.annotations['Hyfer'] == 0].count()
         
     def __len__(self): return len(self.annotations)
     
